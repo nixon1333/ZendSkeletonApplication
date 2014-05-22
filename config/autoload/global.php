37e9703e549576675806d5f9157adedef2623713
Ashraful Islam Nixon <nixon@stsbd.com>
2014-05-23 01:41:22 +0600

    Update global.php

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -15,8 +15,8 @@ return array(
     'db' => array(
          'driver'         => 'Pdo',
          'dsn'            => 'mysql:dbname=zf;host=localhost',
-         'username' => 'nixon',
-         'password' => 'stsbd',
+         'username' => 'your user name here',
+         'password' => 'your password here',
          'driver_options' => array(
              PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
          ),
